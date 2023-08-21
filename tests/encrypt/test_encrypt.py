9fd5fe646560d59583d552993d32406732b9af12
Gustavo-Oliveira7 <gustavo.h.oliveira7531@gmail.com>
2023-08-21 11:37:11 -0300

    encrypt testes feitos

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(0,0)
+    assert encrypt_message('word', 2) == 'wo_rd'
+    assert encrypt_message('word', 3) == 'wor_d'
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message('word','word')
+
     pass
