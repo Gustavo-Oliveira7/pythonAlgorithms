e98c4209c63597eb56330494fd814fda5cf1d962
Gustavo-Oliveira7 <gustavo.h.oliveira7531@gmail.com>
2023-08-21 10:44:44 -0300

    req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+    for i in permanence_period:
+        try:
+            if i[0] <= target_time <= i[1]:
+                count += 1
+        except:
+            return None
+    return count
