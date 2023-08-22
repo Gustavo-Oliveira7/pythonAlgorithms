d6b171978141b55016ee277753699330f5d076cd
Gustavo-Oliveira7 <gustavo.h.oliveira7531@gmail.com>
2023-08-22 12:19:57 -0300

    reduzindo a complexidade do algoritmo

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -6,7 +6,7 @@ def is_anagram(first_string, second_string):
         return is_not_anagram(first_string, second_string)
     if len(firts_str) & len(second_str) == 0:
         return is_not_anagram(first_string, second_string)
-    
+
     for x in firts_str:
         if x not in second_str:
             return is_not_anagram(first_string, second_string)
@@ -20,12 +20,29 @@ def is_not_anagram(first_string, second_string):
 
 
 def my_sort(string):
-    str_list = list(string)
-    for x in range(len(str_list)):
-        min = x
-        for y in range(x + 1, len(str_list)):
-            if str_list[y] < str_list[min]:
-                min = y
-        str_list[x], str_list[min] = str_list[min], str_list[x]
-    str = ''.join(str_list)
-    return str
+    if len(string) <= 1:
+        return string
+    
+    mid = len(string) // 2
+    left = my_sort(string[:mid])
+    right = my_sort(string[mid:])
+    
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    x = y = 0
+    
+    while x < len(left) and y < len(right):
+        if left[x] < right[y]:
+            result.append(left[x])
+            x += 1
+        else:
+            result.append(right[y])
+            y += 1
+    
+    result.extend(left[x:])
+    result.extend(right[y:])
+    return ''.join(result)
+
