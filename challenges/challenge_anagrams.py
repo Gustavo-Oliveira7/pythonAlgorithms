5d7b2f803aaca83f22135f9e40fb967e56b7a44f
Gustavo-Oliveira7 <gustavo.h.oliveira7531@gmail.com>
2023-08-22 11:42:10 -0300

    erro de lint

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -8,9 +8,7 @@ def is_anagram(first_string, second_string):
         return is_not_anagram(first_string, second_string)
     
     for x in firts_str:
-        if x in second_str:
-            x
-        else:
+        if x not in second_str:
             return is_not_anagram(first_string, second_string)
     return (my_sort(firts_str), my_sort(second_str), True)
 
@@ -25,7 +23,7 @@ def my_sort(string):
     str_list = list(string)
     for x in range(len(str_list)):
         min = x
-        for y in range (x + 1, len(str_list)):
+        for y in range(x + 1, len(str_list)):
             if str_list[y] < str_list[min]:
                 min = y
         str_list[x], str_list[min] = str_list[min], str_list[x]
